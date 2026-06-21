1bfd33a2d0f03e7f0b4284410c267b3b145da87f
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-06-21 19:00:53 -0400

    fix: Adequando velocidade para atender requisistos do solicitado no enunciado

--- Exercicios/3 - Java e a Arte da Abstração com Classes e Encapsulamento/2 Rodrigo Car/src/br/com/rodrigo/car/Car.java
+++ Exercicios/3 - Java e a Arte da Abstração com Classes e Encapsulamento/2 Rodrigo Car/src/br/com/rodrigo/car/Car.java
@@ -22,7 +22,7 @@ public class Car {
         this.gear = 0;
         System.out.println("✅ Carro ligado com sucesso!");
     }
-    // ligar.
+    // desligar.
     public void turnOff(){
         if (!this.status) {
             System.out.println("❗ Carro já está desligado.");
@@ -45,7 +45,18 @@ public class Car {
             System.out.printf("Já está na %sª marcha! %n", this.gear);
             return;
         }
-        this.gear += 1;
+
+        // ajuste velocidade para atender requisitos de velocidade
+        if  (this.speed == 0) { this.speed = 0; this.gear = 1;}
+        else if (this.speed == 20){ this.speed = 21; this.gear = 2;}
+        else if (this.speed == 40){ this.speed = 41; this.gear = 3;}
+        else if (this.speed == 60){ this.speed = 61; this.gear = 4;}
+        else if (this.speed == 80){ this.speed = 81; this.gear = 5;}
+        else if (this.speed == 100){ this.speed = 101; this.gear = 6;}
+        else {
+            System.out.printf("❌ Velocidade incorreta para trocar marcha! %s km/h marcha = %s %n", this.speed, this.gear);
+            return;
+        }
         System.out.printf("✅ Subiu para a %sª marcha! E está a %s km/h %n", this.gear, this.speed);
     }
 
@@ -58,31 +69,46 @@ public class Car {
             System.out.printf("❌ Cambio está em neutro! %n");
             return;
         }
-        this.gear -= 1;
-        // FREIO MOTOR
-        if (this.gear == 5 && this.speed > 100) {
-            System.out.println("❗Freio motor foi ativado, " +
-                    "para uma direção suave reduza a velocidade antes de reduzir a marcha");
-            this.speed = 100;
-        } else if (this.gear == 4 && this.speed > 80) {
-            System.out.println("❗Freio motor foi ativado, " +
-                    "para uma direção suave reduza a velocidade antes de reduzir a marcha");
-            this.speed = 80;
-        } else if (this.gear == 3 && this.speed > 60) {
-            System.out.println("❗Freio motor foi ativado, " +
-                    "para uma direção suave reduza a velocidade antes de reduzir a marcha");
-            this.speed = 60;
-        } else if (this.gear == 2 && this.speed > 40) {
-            System.out.println("❗Freio motor foi ativado, " +
-                    "para uma direção suave reduza a velocidade antes de reduzir a marcha");
-            this.speed = 40;
-        } else if (this.gear == 1 && this.speed > 20) {
-            System.out.println("❗Freio motor foi ativado, " +
-                    "para uma direção suave reduza a velocidade antes de reduzir a marcha");
-            this.speed = 20;
-        } else if (this.gear == 0 && this.speed > 0) {
-            System.out.println("❗Câmbio em ponto morto com o carro em movimento! O carro está na banguela.");
+
+        // ajuste velocidade para atender requisitos de velocidade
+        if (this.speed == 101) { this.speed = 100; this.gear = 5; }
+        else if (this.speed == 81) { this.speed = 80; this.gear = 4; }
+        else if (this.speed == 61) { this.speed = 60; this.gear = 3; }
+        else if (this.speed == 41) { this.speed = 40; this.gear = 2; }
+        else if (this.speed == 21) { this.speed = 20; this.gear = 1; }
+        // Se o usuário reduzir da 1ª marcha para o ponto morto a 0 km/h
+        else if (this.speed == 0 && this.gear == 1) { this.gear = 0; }
+        else {
+            System.out.printf("❌ Velocidade incorreta para reduzir a marcha! %s km/h marcha = %s %n", this.speed, this.gear);
+            return; // Impede de reduzir a marcha se a velocidade não for a exata do limite
         }
+
+
+        // FREIO MOTOR
+        // Desativado para atender requisitos
+//        if (this.gear == 5 && this.speed > 100) {
+//        System.out.println("❗Freio motor foi ativado, " +
+//                "para uma direção suave reduza a velocidade antes de reduzir a marcha");
+//        this.speed = 100;
+//    } else if (this.gear == 4 && this.speed > 80) {
+//        System.out.println("❗Freio motor foi ativado, " +
+//                "para uma direção suave reduza a velocidade antes de reduzir a marcha");
+//        this.speed = 80;
+//    } else if (this.gear == 3 && this.speed > 60) {
+//        System.out.println("❗Freio motor foi ativado, " +
+//                "para uma direção suave reduza a velocidade antes de reduzir a marcha");
+//        this.speed = 60;
+//    } else if (this.gear == 2 && this.speed > 40) {
+//        System.out.println("❗Freio motor foi ativado, " +
+//                "para uma direção suave reduza a velocidade antes de reduzir a marcha");
+//        this.speed = 40;
+//    } else if (this.gear == 1 && this.speed > 20) {
+//        System.out.println("❗Freio motor foi ativado, " +
+//                "para uma direção suave reduza a velocidade antes de reduzir a marcha");
+//        this.speed = 20;
+//    } else if (this.gear == 0 && this.speed > 0) {
+//        System.out.println("❗Câmbio em ponto morto com o carro em movimento! O carro está na banguela.");
+//    }
         System.out.printf("✅ Desceu para a %sª marcha! E está a %s km/h %n", this.gear, this.speed);
     }
 
@@ -129,7 +155,8 @@ public class Car {
             System.out.printf("❗ Já está na velocidade máxima de %s km/h! %n", this.speed);
             return;
         }
-        System.out.printf("❌ Você está a %s km/h e está na %sª marcha.%n❗ Ajuste o cambio para acelerar %n", this.speed, this.gear);    }
+        System.out.printf("❌ Você está a %s km/h e está na %sª marcha.%n❗ Ajuste o cambio para acelerar %n", this.speed, this.gear);
+    }
 
     public void decelerate(){
         if (!this.status) {
@@ -156,27 +183,27 @@ public class Car {
             System.out.printf("✅ Velocidade foi para %s km/h, na %sª marcha! %n", this.speed, this.gear);
             return;
         }
-        if ((this.speed > 20 && this.speed <= 40) && this.gear == 2) {
+        if ((this.speed > 21 && this.speed <= 40) && this.gear == 2) {
             this.speed -= this.incrementDecrementSpeed;
             System.out.printf("✅ Velocidade foi para %s km/h, na %sª marcha! %n", this.speed, this.gear);
             return;
         }
-        if ((this.speed > 40 && this.speed <= 60) && this.gear == 3) {
+        if ((this.speed > 41 && this.speed <= 60) && this.gear == 3) {
             this.speed -= this.incrementDecrementSpeed;
             System.out.printf("✅ Velocidade foi para %s km/h, na %sª marcha! %n", this.speed, this.gear);
             return;
         }
-        if ((this.speed > 60 && this.speed <= 80) && this.gear == 4) {
+        if ((this.speed > 61 && this.speed <= 80) && this.gear == 4) {
             this.speed -= this.incrementDecrementSpeed;
             System.out.printf("✅ Velocidade foi para %s km/h, na %sª marcha! %n", this.speed, this.gear);
             return;
         }
-        if ((this.speed > 80 && this.speed <= 100) && this.gear == 5) {
+        if ((this.speed > 81 && this.speed <= 100) && this.gear == 5) {
             this.speed -= this.incrementDecrementSpeed;
             System.out.printf("✅ Velocidade foi para %s km/h, na %sª marcha! %n", this.speed, this.gear);
             return;
         }
-        if ((this.speed > 100 && this.speed <= 120) && this.gear == 6) {
+        if ((this.speed > 101 && this.speed <= 120) && this.gear == 6) {
             this.speed -= this.incrementDecrementSpeed;
             System.out.printf("✅ Velocidade foi para %s km/h, na %sª marcha! %n", this.speed, this.gear);
             return;
@@ -208,6 +235,10 @@ public class Car {
         System.out.printf("✅ Virando direita.%n✅ Velocidade atual é de %s km/h! e está na %sª marcha %n", this.speed, this.gear);
     }
     public void emergencyBreak(){
+        if (!this.status) {
+            System.out.println("❌ Carro desligado.");
+            return;
+        }
         if (!this.status || this.speed == 0 ) {
             System.out.println("❌ Carro já está parado.");
             return;
@@ -220,6 +251,7 @@ public class Car {
     public void getSpeed(){
         if (!this.status) {
             System.out.println("❌ Carro desligado.");
+            return;
         }
         System.out.printf("✅ Velocidade atual é de %s km/h e está na %sª marcha! %n", this.speed, this.gear);
     }
