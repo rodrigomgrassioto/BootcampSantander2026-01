ae018142e347b975b0297842522c5610ffaffce4
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-06-24 17:27:41 -0400

    test: Testes realizados com sucesso.

--- Exercicios/4 - Herança e Polimorfismo em Java/2-RodrigoUsersControl/src/Main.java
+++ Exercicios/4 - Herança e Polimorfismo em Java/2-RodrigoUsersControl/src/Main.java
@@ -1,13 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import com.devrodrigo.userscontrol.Attendant;
+import com.devrodrigo.userscontrol.Manager;
+import com.devrodrigo.userscontrol.Seller;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+void main() {
+    System.out.println("✅ GERENTE!");
+    Manager gerente = new Manager("Rodrigo", "tst@devrodrigo.com.", "123");
+    // teste com senha errada
+    gerente.changePersonalData("Rodrigo Medeiros Grassioto", "falecom@devrodrigo.com", "555");
+    // Atualizando dados
+    gerente.changePersonalData("Rodrigo Medeiros Grassioto", "falecom@devrodrigo.com", "123");
+    // teste com senha errada
+    gerente.changePassword("568", "987");
+    // trocando senha
+    gerente.changePassword("123", "987");
+    // relatório financeiro
+    gerente.financialReport();
+    // relatório de vendas
+    gerente.salesReport();
+
+    System.out.printf("%n%n✅ VENDEDOR!%n");
+    Seller vendedor = new Seller("João da Silva", "joao@supervendas.com.br", "777", 8);
+    vendedor.newSale();
+    vendedor.consultSales();
+
+    System.out.printf("%n%n✅ ATENDENTE!%n");
+    Attendant atendente = new Attendant("Carlos Santos", "carlos@supervendas.com.br", "789", 1200);
+    atendente.receivePayment(300);
+    // com valore errado
+    atendente.closeTheRegister(1200);
+    // fechando caixa
+    atendente.closeTheRegister(1500);
 }
