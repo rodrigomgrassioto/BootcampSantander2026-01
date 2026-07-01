9a6a46d8e86afe283e453a929559cf8518164b34
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-01 12:40:53 -0400

    feat: Criado classe para formatar multiplos tipos de telefones e record para padronizar a resposta.

--- Exercicios/6 - Praticando com Collections e Outras Classes Úteis do Java/RodrigoFoneFormater/src/Main.java
+++ Exercicios/6 - Praticando com Collections e Outras Classes Úteis do Java/RodrigoFoneFormater/src/Main.java
@@ -5,24 +5,22 @@ Scanner scanner = new Scanner(System.in);
 
 void main() {
 
-    System.out.println("==============================");
-    System.out.println("========Rodrigo Calc==========");
-    System.out.printf ("==============================%n%n");
+    System.out.println("================================================");
+    System.out.println("========Rodrigo Formatador de Telefone==========");
+    System.out.printf ("================================================%n%n");
 
-    String number = "";
     while (true){
-        System.out.printf("DDD + telefone (ZERO para sair): %s", number);
-        number = scanner.nextLine();
+        System.out.print("DDD + telefone (ZERO para sair): ");
+        String number = scanner.nextLine();
         if (number.equals("0")) System.exit(0);
 
         PhoneResponse phoneResponse = PhoneFormater.formatPhone(number);
-        if (!phoneResponse.success()) number = phoneResponse.formattedNumber();
 
-        System.out.printf("%s%n", phoneResponse);
+        if (!phoneResponse.success()) {
+            System.out.printf("❌ %s%n%n", phoneResponse.message());
+            continue;
+        }
+        System.out.println("\n✅ Telefone processado com sucesso!");
+        System.out.printf("Resultado estruturado (JSON):%n%s%n%n", phoneResponse.toJson());
     }
-
-
-
-
-
 }
--- Exercicios/6 - Praticando com Collections e Outras Classes Úteis do Java/RodrigoFoneFormater/src/com/devrodrigo/phone/PhoneFormater.java
+++ Exercicios/6 - Praticando com Collections e Outras Classes Úteis do Java/RodrigoFoneFormater/src/com/devrodrigo/phone/PhoneFormater.java
@@ -10,27 +10,40 @@ public final class PhoneFormater {
             return new PhoneResponse("", "Desconhecido", false, "Número não pode ser vazio.");
         }
 
-        // Retirar todos caractéres que não sejam números
-        String apenasNumeros = unformattedNumber.replaceAll("\\D", "");
+        // Apenas números, tmbm vai atender o requisito de: se receber nr formatado deverá retornar formatado
+        String onlyNumbers = unformattedNumber.replaceAll("\\D", "");
 
-        // Celular
-        if (apenasNumeros.length() == 11) {
-            String formatado = apenasNumeros.replaceAll("(\\d{2})(\\d{1})(\\d{4})(\\d{4})", "($1) $2 $3-$4");
-            return new PhoneResponse(formatado, "Celular", true, "Telefone celular processado com sucesso.");
+        // DDD + Celular
+        if (onlyNumbers.length() == 11) {
+//            String formatado = onlyNumbers.replaceAll("(\\d{2})(\\d{1})(\\d{4})(\\d{4})", "($1) $2 $3-$4"); // com espaço no nono dígito
+            String formatado = onlyNumbers.replaceAll("(\\d{2})(\\d{5})(\\d{4})", "($1) $2-$3");
+            return new PhoneResponse(formatado, "DDD+CELULAR", true, "DDE + celular processado com sucesso.");
         }
 
-        // Fixo
-        if (apenasNumeros.length() == 10) {
-            String formatado = apenasNumeros.replaceAll("(\\d{2})(\\d{4})(\\d{4})", "($1) $2-$3");
-            return new PhoneResponse(formatado, "Fixo", true, "Telefone fixo processado com sucesso.");
+        // DDD + Fixo
+        if (onlyNumbers.length() == 10) {
+            String formatado = onlyNumbers.replaceAll("(\\d{2})(\\d{4})(\\d{4})", "($1) $2-$3");
+            return new PhoneResponse(formatado, "DDD+FIXO", true, "DDD + fixo processado com sucesso.");
         }
 
-        // Se não tiver 10 ou 11 digitos, Falha de Validação
+        // Apenas Cel - quase esqueci do requisito sem DDD
+        if (onlyNumbers.length() == 9){
+            String formatado = onlyNumbers.replaceAll("(\\d{5})(\\d{4})", "$1-$2");
+            return new PhoneResponse(formatado, "APENAS_CELULAR", true, "Celular sem DDD processado com sucesso.");
+        }
+
+        // Apenas fixo
+        if (onlyNumbers.length() == 8){
+            String formatado = onlyNumbers.replaceAll("(\\d{4})(\\d{4})", "$1-$2");
+            return new PhoneResponse(formatado, "APENAS_FIXO", true, "Telefone fixo sem DDD processado com sucesso.");
+        }
+
+        // Se não tiver 8 e 11 digitos, Falha de Validação
         return new PhoneResponse(
                 unformattedNumber,
-                "Inválido",
+                "INVÁLIDO",
                 false,
-                "Falha: O telefone (+ DDD) precisa ter 10 ou 11 números (Fixo/Celular):"
+                "Falha: O telefone precisa ter entre 8 e 11 números (Fixo/Celular Com/Sem DDD):"
         );
     }
 }
