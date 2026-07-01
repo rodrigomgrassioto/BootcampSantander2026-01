fea15071ecccb6dc2b3c1770bba9eda75dab86b9
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-01 14:34:22 -0400

    wip: Editando Main 3

--- Exercicios/6 - Praticando com Collections e Outras Classes Úteis do Java/3-RodrigoConverterToJsonXmlYaml/src/Main.java
+++ Exercicios/6 - Praticando com Collections e Outras Classes Úteis do Java/3-RodrigoConverterToJsonXmlYaml/src/Main.java
@@ -6,18 +6,20 @@ void main(){
     System.out.printf ("===================================%n%n");
 
     System.out.println("Bem vindo!");
-    var numberFields = -1;
+    int numberFields = -1;
     while (numberFields <= 0){
-        System.out.print("Quantidade de campos: (Zero para sair) ");
-        numberFields = scanner.nextInt();
+        System.out.println("Para encerrar o programa digite '0' (ZERO)");
+        numberFields = lerNumeroSeguro("Quantidade de campos: ", "int").intValue();
         if (numberFields == 0) System.exit(1);
     }
-    System.out.print("Digite a chave;valor do campo: (Zero para sair) ");
+    System.out.println("Para encerrar o programa digite '0' (ZERO)");
+    System.out.print("Digite a chave e valor(separado por ';'): ");
     var keyValue  = scanner.nextLine();
     if (keyValue.equals("0")) System.exit(1);
 
     int option = -1;
     while (option <= 0 || option > 4){
+        System.out.println("Para encerrar o programa digite '0' (ZERO)");
         System.out.println("==== Escolha: ===");
         System.out.println("1 -> Inteiro");
         System.out.println("2 -> Decima");
@@ -25,20 +27,21 @@ void main(){
         System.out.println("4 -> Verdadeiro ou falso");
         option = lerNumeroSeguro("Valor escolhido:", "int").intValue();
         if (option == 0) System.exit(0);
-        if (option != 1 && option != 2){
-            System.out.println("Opção inválida.");
-        }
+        else System.out.printf("digitou: %s", option);
+//        if (option != 1 && option != 2){
+//            System.out.println("Opção inválida.");
+//        }
     }
-    var selectedOperation = Operation.values()[option - 1];
-
-    System.out.print("Digite os números separados por virgula (ex: 1,2,3,4,5): ");
-    // aceita usuário digitar espaço, mas remove este espaço antes de gerar o array.
-    var numbers = scanner.nextLine().replace(" ", "");
-    var numberArray = Arrays.stream(numbers.split(","))
-            .mapToLong(Long::parseLong)
-            .toArray();
-
-    var result = selectedOperation.getOperationCallbackTest().exec(numberArray);
-    var operationShow = numbers.replaceAll(",", selectedOperation.getSignal());
-    System.out.printf("Resultado de %s = %s%n", operationShow,  result);
+//    var selectedOperation = Operation.values()[option - 1];
+//
+//    System.out.print("Digite os números separados por virgula (ex: 1,2,3,4,5): ");
+//    // aceita usuário digitar espaço, mas remove este espaço antes de gerar o array.
+//    var numbers = scanner.nextLine().replace(" ", "");
+//    var numberArray = Arrays.stream(numbers.split(","))
+//            .mapToLong(Long::parseLong)
+//            .toArray();
+//
+//    var result = selectedOperation.getOperationCallbackTest().exec(numberArray);
+//    var operationShow = numbers.replaceAll(",", selectedOperation.getSignal());
+//    System.out.printf("Resultado de %s = %s%n", operationShow,  result);
 }
