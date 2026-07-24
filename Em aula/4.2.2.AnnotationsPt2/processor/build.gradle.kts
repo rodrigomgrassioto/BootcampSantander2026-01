41b7d96c62895a47258f614a8e1e2d733459c0a7
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-24 18:07:17 -0400

    feat: Removido javax.annotation para usar o autoservice da google

--- Em aula/4.2.2.AnnotationsPt2/processor/build.gradle.kts
+++ Em aula/4.2.2.AnnotationsPt2/processor/build.gradle.kts
@@ -9,6 +9,12 @@ repositories {
     mavenCentral()
 }
 
+val autoServiceVersion = "1.1.1"
 dependencies {
+    compileOnly("com.google.auto.service:auto-service:$autoServiceVersion")
+    annotationProcessor("com.google.auto.service:auto-service:$autoServiceVersion")
+    implementation("com.squareup:javapoet:1.13.0")
 
+    compileOnly(project(":annotation"))
+    implementation(project(":annotation"))
 }
