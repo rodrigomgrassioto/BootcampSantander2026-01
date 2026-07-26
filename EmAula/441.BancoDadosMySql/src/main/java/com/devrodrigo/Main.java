63d681f1d8e41cd5ca4141ba6586ccb40d860c3b
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-25 21:56:35 -0400

    feat: Adiciona dados na base

--- EmAula/441.BancoDadosMySql/src/main/java/com/devrodrigo/Main.java
+++ EmAula/441.BancoDadosMySql/src/main/java/com/devrodrigo/Main.java
@@ -1,12 +1,26 @@
 package com.devrodrigo;
 
-import org.flywaydb.core.Flyway;
+import com.devrodrigo.persistence.EmployeeDAO;
+import com.devrodrigo.persistence.entity.EmployeeEntity;
+//import org.flywaydb.core.Flyway;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 public class Main {
     static void main(){
-        final var flyway = Flyway.configure()
-                .dataSource("jdbc:mysql://localhost:3306/bcsantander_441aula","root","")
-                .load();
-        flyway.migrate();
+//        final var flyway = Flyway.configure()
+//                .dataSource("jdbc:mysql://localhost:3306/bcsantander_441aula","root","")
+//                .load();
+//        flyway.migrate();
+
+        final var employeeDAO = new EmployeeDAO();
+        final var employee = new EmployeeEntity();
+        employee.setName("Kiki");
+        employee.setSalary(new BigDecimal("0.00"));
+        employee.setBirthday(OffsetDateTime.now().minusYears(6));
+        System.out.print(employee);
+        employeeDAO.insert(employee);
+        System.out.print(employee);
     }
 }
--- EmAula/441.BancoDadosMySql/src/main/java/com/devrodrigo/persistence/EmployeeDAO.java
+++ EmAula/441.BancoDadosMySql/src/main/java/com/devrodrigo/persistence/EmployeeDAO.java
@@ -1,11 +1,32 @@
 package com.devrodrigo.persistence;
 
 import com.devrodrigo.persistence.entity.EmployeeEntity;
+import com.mysql.cj.jdbc.StatementImpl;
 
+import java.sql.SQLException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class EmployeeDAO {
     public void insert(final EmployeeEntity entity){
+        try(
+            var connection = ConnectUtil.getConnection();
+            var statement = connection.createStatement()
+            ) {
+            var sql = "INSERT INTO employees (name, salary, birthday) values ('"+
+                    entity.getName() + "', " +
+                    entity.getSalary().toString() + ", "+
+                    "'" + formatOffsetDateTime(entity.getBirthday()) + "')";
+            statement.executeUpdate(sql);
+//            System.out.printf("Foram afetados %s, no banco de dados", statement.getUpdateCount());
+
+            if (statement instanceof StatementImpl impl)
+                entity.setId(impl.getLastInsertID());
+
+        } catch (SQLException exception){
+            exception.printStackTrace();
+        }
 
     }
     public void update(final EmployeeEntity entity){
@@ -21,5 +42,9 @@ public class EmployeeDAO {
 
     public EmployeeEntity findById(final long id){
         return  null;
+    }
+
+    private String formatOffsetDateTime(final OffsetDateTime dateTime){
+        return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }
