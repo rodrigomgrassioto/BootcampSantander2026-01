4155f797b59e35df3c29190dd487bfc8f208ac44
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-25 20:46:19 -0400

    feat: Cria entity e DAO do empregado

---
+++ EmAula/441.BancoDadosMySql/src/main/java/com/devrodrigo/persistence/EmployeeDAO.java
@@ -0,0 +1,25 @@
+package com.devrodrigo.persistence;
+
+import com.devrodrigo.persistence.entity.EmployeeEntity;
+
+import java.util.List;
+
+public class EmployeeDAO {
+    public void insert(final EmployeeEntity entity){
+
+    }
+    public void update(final EmployeeEntity entity){
+
+    }
+    public void delete(final long id){
+
+    }
+
+    public List<EmployeeEntity> findAll (){
+        return null;
+    }
+
+    public EmployeeEntity findById(final long id){
+        return  null;
+    }
+}
