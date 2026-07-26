4dd57bfad3fa0a0daf79d6268dd89e4d628e5ee6
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-25 22:19:44 -0400

    feat: usa o PreparedStatement para melhorar concatenação. Pesquisa pessoal

--- EmAula/441.BancoDadosMySql/src/main/java/com/devrodrigo/persistence/EmployeeDAO.java
+++ EmAula/441.BancoDadosMySql/src/main/java/com/devrodrigo/persistence/EmployeeDAO.java
@@ -4,21 +4,31 @@ import com.devrodrigo.persistence.entity.EmployeeEntity;
 import com.mysql.cj.jdbc.StatementImpl;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class EmployeeDAO {
     public void insert(final EmployeeEntity entity){
+        // Pesquisa pessoal, usando o PreparedStatement para melhorar a concatenação
+        final String sql = "INSERT INTO employees (name, salary, birthday) VALUES (?, ?, ?)";
         try(
             var connection = ConnectUtil.getConnection();
-            var statement = connection.createStatement()
-            ) {
-            var sql = "INSERT INTO employees (name, salary, birthday) values ('"+
-                    entity.getName() + "', " +
-                    entity.getSalary().toString() + ", "+
-                    "'" + formatOffsetDateTime(entity.getBirthday()) + "')";
-            statement.executeUpdate(sql);
+//            var statement = connection.createStatement()
+            var statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
+        ) {
+//            var sql = "INSERT INTO employees (name, salary, birthday) values ('"+
+//                    entity.getName() + "', " +
+//                    entity.getSalary().toString() + ", "+
+//                    "'" + formatOffsetDateTime(entity.getBirthday()) + "')";
+
+            // inserindo as variáveis
+            statement.setString(1, entity.getName());
+            statement.setBigDecimal(2, entity.getSalary());
+            statement.setObject(3, entity.getBirthday());
+
+            statement.executeUpdate();
 //            System.out.printf("Foram afetados %s, no banco de dados", statement.getUpdateCount());
 
             if (statement instanceof StatementImpl impl)
