7e76cbeebb5a39af6b560e2653ca6723c2aadcdd
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-30 16:03:32 -0400

    test: Test controller

--- EmAula/512-taskmanager/src/main/java/com/devrodrigo/taskmanager/infrastructure/http/TaskController.java
+++ EmAula/512-taskmanager/src/main/java/com/devrodrigo/taskmanager/infrastructure/http/TaskController.java
@@ -1,8 +1,27 @@
 package com.devrodrigo.taskmanager.infrastructure.http;
 
 
+import com.devrodrigo.taskmanager.application.CreateTaskUseCase;
+import com.devrodrigo.taskmanager.application.input.CreateTaskInput;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
+    private final CreateTaskUseCase createTaskUseCase;
+
+    public TaskController(CreateTaskUseCase createTaskUseCase) {
+        this.createTaskUseCase = createTaskUseCase;
+    }
+
+    @PostMapping
+    void create() {
+        var input = new CreateTaskInput("Renovar passaporte", Optional.empty());
+        createTaskUseCase.execute(input);
+    }
 }
