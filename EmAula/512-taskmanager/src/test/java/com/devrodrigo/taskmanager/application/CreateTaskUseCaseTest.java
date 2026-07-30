71c6af7820aed61d04593a26c30eee1852904a6c
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-30 11:27:46 -0400

    wip: Teste funcionando, agora com anotações MOCK. Mais info comentado na linha 46

--- EmAula/512-taskmanager/src/test/java/com/devrodrigo/taskmanager/application/CreateTaskUseCaseTest.java
+++ EmAula/512-taskmanager/src/test/java/com/devrodrigo/taskmanager/application/CreateTaskUseCaseTest.java
@@ -2,18 +2,32 @@ package com.devrodrigo.taskmanager.application;
 
 import com.devrodrigo.taskmanager.application.input.CreateTaskInput;
 import com.devrodrigo.taskmanager.application.output.TaskOutput;
+import com.devrodrigo.taskmanager.domain.Task;
+import com.devrodrigo.taskmanager.domain.TaskRepository;
 import com.devrodrigo.taskmanager.infrastructure.repository.InMemoryTaskRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class CreateTaskUseCaseTest {
-    @Autowired
+    @Mock
+    TaskRepository repository;
+
+//    @Autowired // não necessário após usar o mock
+    @InjectMocks
     CreateTaskUseCase useCase;
 
     /* não é mais necessário esse bloco após anotar
@@ -29,11 +43,21 @@ class CreateTaskUseCaseTest {
     void execute(){
         var input = new CreateTaskInput("Estudar java", Optional.of("Antes reiniciar notebook"));
 
+        /* Simula chamada do repositório, onde responde com o argumento que entrou. ????
+        Podendo simular resposta do repository sem realmente salvar no banco de dados, ou
+        se for uma API simular a resposta dela */
+        when(repository.save(any(Task.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+
         TaskOutput output = useCase.execute(input);
 
         assertNotNull(output);
         assertNotNull(output.id());
         assertEquals("Estudar java", output.title());
         assertEquals(Optional.of("Antes reiniciar notebook"), output.description());
+
+        // verfica se o repository foi chamado apenas 1 vez
+        verify(repository, times(1)).save(any(Task.class));
     }
 }
