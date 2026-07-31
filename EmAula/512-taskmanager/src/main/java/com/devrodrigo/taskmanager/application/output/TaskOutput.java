c4690f0e1a961222cb021f6f2f9055639a88a569
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-31 14:12:42 -0400

    fix: Corrige retorno com {                            "id" : "TaskId[id=2330a4ef-8597-49fb-8db3-365aa7d42634]",                            "title" : "Aprender Spring",                            ...

--- EmAula/512-taskmanager/src/main/java/com/devrodrigo/taskmanager/application/output/TaskOutput.java
+++ EmAula/512-taskmanager/src/main/java/com/devrodrigo/taskmanager/application/output/TaskOutput.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 public record TaskOutput(String id, String title, Optional<String> description, String status) {
 
     public static TaskOutput from(Task task) {
-        return new TaskOutput(task.getId().toString(),
+        return new TaskOutput(task.getId().id().toString(),
                 task.getTitle(),
                 task.getDescription(),
                 task.getStatus().name());
