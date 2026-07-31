9dee817c1100962d0fac59f0bd6c3ef8c728ec89
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-07-31 09:29:44 -0400

    feat: Cria GlobalExceptionHandler

---
+++ EmAula/512-taskmanager/src/main/java/com/devrodrigo/taskmanager/infrastructure/http/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.devrodrigo.taskmanager.infrastructure.http;
+
+import com.devrodrigo.taskmanager.domain.TaskNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(TaskNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+
+    public String handleTaskNotFoundException(TaskNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
