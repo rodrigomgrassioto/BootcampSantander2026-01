5e0f5e5552004b431504c46e22595663865bc6d3
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-08-05 00:53:28 -0400

    wip: Persistencia do ticketin no Postgres

---
+++ EmAula/522-marketPlaceEventos/src/main/java/com/devrodrigo/_22marketplaceeventos/ticketing/infrastructure/TicketingConfiguration.java
@@ -0,0 +1,77 @@
+package com.devrodrigo._22marketplaceeventos.ticketing.infrastructure;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties;
+import org.springframework.boot.jpa.EntityManagerFactoryBuilder;
+import org.springframework.boot.jpa.autoconfigure.JpaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.LinkedHashMap;
+
+@Configuration(proxyBeanMethods = false)
+@EnableJpaRepositories(
+    basePackages = "com.devrodrigo._22marketplaceeventos.ticketing",
+    entityManagerFactoryRef = "ticketingEntityManagerFactory",
+    transactionManagerRef = "ticketingTransactionManager"
+)
+public class TicketingConfiguration {
+
+    @Qualifier("ticketing")
+    @Bean(defaultCandidate = false)
+    @ConfigurationProperties("ticketing.datasource")
+    public DataSourceProperties ticketingDataSourceProprieties() {
+        return new DataSourceProperties();
+    }
+
+    @Qualifier("ticketing")
+    @Bean(defaultCandidate = false)
+    @ConfigurationProperties("ticketing.datasource.configuration")
+    public HikariDataSource ticketingDataSource(
+            @Qualifier("ticketing") DataSourceProperties properties
+    ) {
+        return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
+
+    @Qualifier("ticketing")
+    @Bean(defaultCandidate = false)
+    @ConfigurationProperties("ticketing.jpa")
+    public JpaProperties ticketingJpaProperties() {
+        return new JpaProperties();
+    }
+
+    @Qualifier("ticketing")
+    @Bean(defaultCandidate = false)
+    public LocalContainerEntityManagerFactoryBean ticketingEntityManagerFactoryProprieties(
+            @Qualifier("ticketing") DataSource dataSource,
+            @Qualifier("ticketing") JpaProperties jpaProperties
+    ) {
+        var builder = new EntityManagerFactoryBuilder(
+                new HibernateJpaVendorAdapter(),
+                x -> new LinkedHashMap<>(jpaProperties.getProperties()),
+                null
+        );
+
+        return builder
+                .dataSource(dataSource)
+                .packages("com.devrodrigo.ticketing")
+                .persistenceUnit("ticketing")
+                .build();
+    }
+
+    @Qualifier("ticketing")
+    @Bean
+    public PlatformTransactionManager ticketingTransactionManager(
+            @Qualifier("ticketing") LocalContainerEntityManagerFactoryBean emf){
+        return new JpaTransactionManager(emf.getObject());
+    }
+
+}
