41985acf5754b9e294b38ae30d2fcc4c8bfddf95
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-08-07 06:41:06 -0400

    feat: Login via api funcional, retornando apenas 200, sem token, se cookie

--- EmAula/533-proposal_manager/src/main/java/com/devrodrigo/proposal_manager/auth/infrastructure/security/SecurityConfig.java
+++ EmAula/533-proposal_manager/src/main/java/com/devrodrigo/proposal_manager/auth/infrastructure/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devrodrigo.proposal_manager.auth.infrastructure;
+package com.devrodrigo.proposal_manager.auth.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,16 +14,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+    SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            RestUsernamePasswordAuthenticationFilter restUsernamePasswordAuthenticationFilter) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // desativa token csrf usado em forms
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterAt(restUsernamePasswordAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
