9e449d6ab111764f7818a8d7fb45d797f748aed1
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-08-08 21:10:04 -0400

    fix: Corrige retono dos IDs

--- EmAula/533-proposal_manager/src/main/java/com/devrodrigo/proposal_manager/proposal/application/output/ProposalOutput.java
+++ EmAula/533-proposal_manager/src/main/java/com/devrodrigo/proposal_manager/proposal/application/output/ProposalOutput.java
@@ -7,10 +7,10 @@ import java.util.Optional;
 
 public record ProposalOutput(String id, String title, Optional<String> description, String ownerId, String ownerName) {
     public static ProposalOutput from(Proposal proposal) {
-        return new ProposalOutput(proposal.getId().toString(),
+        return new ProposalOutput(proposal.getId().id().toString(),
                 proposal.getTitle(),
                 proposal.getDescription(),
-                proposal.getOwner().id().toString(),
+                proposal.getOwner().id().id().toString(),
                 proposal.getOwner().name());
     }
 }
