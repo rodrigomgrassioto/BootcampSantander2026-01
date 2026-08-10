8859d66dfea2b72d9ca499483a0379cca2d3874f
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-08-10 18:10:37 -0400

    chore: Configura url globalmente e nivel de log.

--- EmAula/542-compliance/src/main/java/com/devrodrigo/_542_compliance/infrastructure/persistence/rest/client/SanctionClient.java
+++ EmAula/542-compliance/src/main/java/com/devrodrigo/_542_compliance/infrastructure/persistence/rest/client/SanctionClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "sanction-client", url = "https://a2fa188f-7b2b-41fa-b4d4-18aae59f1fef.mock.pstmn.io")
+@FeignClient(name = "sanction-client")
 public interface SanctionClient {
     @GetMapping("/sanctions/companies/{registrationNumber}")
     void getCompanyRisk(@PathVariable String registrationNumber);
