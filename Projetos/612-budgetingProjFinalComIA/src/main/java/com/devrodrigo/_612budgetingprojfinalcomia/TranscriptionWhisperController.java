dd37d1fa750f83211b42858341735cfa1b9283a9
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-08-18 21:47:00 -0400

    wip: Recebendo retorno em formato Json dentro da chave text

--- Projetos/612-budgetingProjFinalComIA/src/main/java/com/devrodrigo/_612budgetingprojfinalcomia/TranscriptionWhisperController.java
+++ Projetos/612-budgetingProjFinalComIA/src/main/java/com/devrodrigo/_612budgetingprojfinalcomia/TranscriptionWhisperController.java
@@ -2,6 +2,7 @@ package com.devrodrigo._612budgetingprojfinalcomia;
 
 import org.springframework.ai.audio.transcription.TranscriptionModel;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,8 @@ import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.http.HttpClient;
+import java.time.Duration;
 
 @RestController
 @RequestMapping("api")
@@ -21,6 +24,13 @@ public class TranscriptionWhisperController {
     public TranscriptionWhisperController() {
         this.restClient = RestClient.builder()
                 .baseUrl("http://192.168.100.10:8080")
+                .requestFactory(
+                        new JdkClientHttpRequestFactory(
+                                HttpClient.newBuilder()
+                                        .connectTimeout(Duration.ofSeconds(10))
+                                        .build()
+                        )
+                )
                 .build();
     }
 
