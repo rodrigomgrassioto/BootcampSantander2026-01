942bb5b8deb5181ff91d6f18b4042a8f0f8d8915
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-08-18 21:41:32 -0400

    wip: Converter para usar Whisper - 2 Erro de time-out

--- Projetos/612-budgetingProjFinalComIA/src/main/java/com/devrodrigo/_612budgetingprojfinalcomia/TranscriptionWhisperController.java
+++ Projetos/612-budgetingProjFinalComIA/src/main/java/com/devrodrigo/_612budgetingprojfinalcomia/TranscriptionWhisperController.java
@@ -2,10 +2,12 @@ package com.devrodrigo._612budgetingprojfinalcomia;
 
 import org.springframework.ai.audio.transcription.TranscriptionModel;
 import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -13,18 +15,31 @@ import java.io.IOException;
 @RestController
 @RequestMapping("api")
 public class TranscriptionWhisperController {
-    private final TranscriptionModel transcriptionModel;
 
-    public TranscriptionWhisperController(TranscriptionModel transcriptionModel) {
-        this.transcriptionModel = transcriptionModel;
+    private final RestClient restClient;
+
+    public TranscriptionWhisperController() {
+        this.restClient = RestClient.builder()
+                .baseUrl("http://192.168.100.10:8080")
+                .build();
     }
 
     @PostMapping(value = "/transcribe", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     String transcribe(@RequestParam("file") MultipartFile file) throws IOException {
         System.out.println("file é: "+file);
-        var resource = file.getResource();
-        System.out.println("Resource é: "+resource);
-        System.out.println("Resource.getFilePath é: "+resource.getFilePath());
-        return transcriptionModel.transcribe(resource);
+
+        var parts = new LinkedMultiValueMap<String, Object>();
+        parts.add("file", file.getResource());
+
+        var response = restClient.post()
+                .uri("/inference")
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(parts)
+                .retrieve()
+                .body(String.class);
+
+        System.out.println("Resposta do Whisper: " + response);
+
+        return response;
     }
 }
