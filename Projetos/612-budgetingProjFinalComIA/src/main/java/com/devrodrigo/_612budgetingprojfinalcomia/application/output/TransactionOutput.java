9a75b1f003ad7dacb3ae02886407645f66d693dd
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-08-20 16:29:19 -0400

    feat: Fix corrige retorno dos valores. Antes 10 reais retornava 1000.0

--- Projetos/612-budgetingProjFinalComIA/src/main/java/com/devrodrigo/_612budgetingprojfinalcomia/application/output/TransactionOutput.java
+++ Projetos/612-budgetingProjFinalComIA/src/main/java/com/devrodrigo/_612budgetingprojfinalcomia/application/output/TransactionOutput.java
@@ -10,6 +10,9 @@ public record TransactionOutput(String id, String description, String category, 
         return new TransactionOutput(transaction.getId().uuid().toString(),
                 transaction.getDescription(),
                 transaction.getCategory().name(),
-                BigDecimal.valueOf(transaction.getAmount()).setScale(2, RoundingMode.HALF_UP).doubleValue());
+                BigDecimal.valueOf(transaction.getAmount())
+                        .divide(BigDecimal.valueOf(100))
+                        .setScale(2, RoundingMode.HALF_UP)
+                        .doubleValue());
     }
 }
